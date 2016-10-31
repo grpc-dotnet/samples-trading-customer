6c783f9383ca61a2b103171d0e7752533e0b6afe
liujinfu <liujinfu@fang.com>
2016-10-31 15:23:35 +0800

    add order.proto

---
+++ src/Cusomter/Samples.Trading.Customer.Client/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Samples.Trading.Customer.Client
+{
+    public class Class1
+    {
+    }
+}
